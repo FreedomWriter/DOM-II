9ff8756c5f8de9f82dd0b3fddd99504420e65e5d
FreedomWriter <natalienetworks@gmail.com>
2019-09-10 13:51:48 -0500

    still working on stretch

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -9,56 +9,39 @@ blocks.forEach(() => {
             if (blocks.indexOf(block) !== 0 ) {
                 blockDiv.prepend(block);  
             }  
-        });
-        block.addEventListener('mousedown', (e) => {
-            block.style.position= 'relative'; 
-            block.style.left = '0px';
-            block.style.left = parseInt(block.style.left) + 10 + 'px';
 
-            let start = Date.now();
-
-            let  timer = setInterval(function() {
-                let timePassed = Date.now() - start;
-
-                if (timePassed >= 20000) {
-                    clearInterval(timer);
-                } 
-                
-                draw(timePassed);
-
-            }, 20);
-
-            function draw(timePassed) {
-                block.style.left = timePassed / 5 + 'px';
-            }
-
-            
-            
-            console.log('testing mouse down');
-            
-        });
+            block.addEventListener('mousedown', (e) => {
+                block.style.position= 'relative'; 
+                block.style.left = '0px';
+                block.style.left = parseInt(block.style.left) + 10 + 'px';
     
-        block.addEventListener('mouseup', (e) => {
-            block.style.position= 'relative'; 
-            block.style.left = '0px';
-            let start = Date.now();
-
-            let  timer = setInterval(function() {
-                let timePassed = Date.now() - start;
-
-                if (timePassed >= 0) {
+                let start = Date.now();
+    
+                let  timer = setInterval(function() {
+                    let timePassed = Date.now() - start;
+    
+                    if (timePassed >= 6000) {
+                        clearInterval(timer);
+                        block.style.position= 'relative'; 
+                        block.style.left = '0px';
+                    } 
+                    
+                    draw(timePassed);
+    
+                }, 10);
+    
+                function draw(timePassed) {
+                    block.style.left = timePassed / 5 + 'px';
+                }
+                console.log('testing mouse down'); 
+                block.addEventListener('mouseup', (e) => {
                     clearInterval(timer);
-                    block.style.left = '0px';
-                } 
-                
-                draw(timePassed);
-
-            }, 0);
-            function draw(timePassed) {
-                block.style.left = timePassed / 5 + 'px';
-            }
-            console.log('testinggg mouse uppp');
-            
+                    console.log('testinggg mouse uppp'); 
+                });
+            });
         });
+        
+    
+        
     }
 });
